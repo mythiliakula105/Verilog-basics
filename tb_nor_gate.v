0b674efd878a29d2f9a333d67ac390f0d12e0c67
Akula Venkata Mythili <257320428+mythiliakula105@users.noreply.github.com>
2026-02-20 19:18:04 +0530

    Create tb_nor_gate.v

---
+++ tb_nor_gate.v
@@ -0,0 +1,17 @@
+module tb_nor_gate;
+    reg a, b;
+    wire y;
+
+    nor_gate uut (.a(a), .b(b), .y(y));
+
+    initial begin
+        $dumpfile("dump_nor.vcd");
+        $dumpvars(0, tb_nor_gate);
+
+        a = 0; b = 0;
+        #10 a = 0; b = 1;
+        #10 a = 1; b = 0;
+        #10 a = 1; b = 1;
+        #10 $finish;
+    end
+endmodule
